2979ec75b5ab73e57365a7e851c12d13199628c6
Ben Roesch <bcroesch@gmail.com>
2012-08-23 19:03:58 -0500

    Add user specs for is_admin/member_of_league? methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe User do
-  
+  it "#is_admin_of_league? returns the correct bool value" do
+    u = FactoryGirl.create(:user)
+    l = FactoryGirl.create(:league)
+    u.is_admin_of_league?(l).should be_false
+
+    m = l.memberships.build
+    m.user = u
+    m.role = Membership::ROLES[:admin]
+    m.save
+    
+    u.is_admin_of_league?(l).should be_true
+  end
+
+  it "#is_member_of_league? returns the correct bool value" do
+    u = FactoryGirl.create(:user)
+    l = FactoryGirl.create(:league)
+    u.is_member_of_league?(l).should be_false
+
+    l.users << u
+
+    u.is_member_of_league?(l).should be_true
+  end
 end
