cfd65b98e529b44f1da9370671f0964c0499c68e
Ben Roesch <bcroesch@gmail.com>
2013-03-06 16:45:53 -0600

    Update location of push notification certificate in prod

--- lib/ffp/push_notifications.rb
+++ lib/ffp/push_notifications.rb
@@ -4,7 +4,11 @@ module FFP
 
     def grocer
       @grocer ||= begin
-                    cert_location = Rails.env.production? ? "" : "#{Rails.root}/lib/ios_push_cert_dev.pem" 
+                    cert_location = if Rails.env.production? 
+                      "/var/www/apps/prophets/ProphetsProdPushNotificationCertificate.pem"
+                    else
+                      "#{Rails.root}/lib/ios_push_cert_dev.pem" 
+                    end
                     connection = Grocer::PushConnection.new(certificate: cert_location)
                     connection.connect
                     Grocer::Pusher.new(connection)
