f9257e49a2af70015b93c48105234e82a74aef71
Ben Roesch <bcroesch@gmail.com>
2013-06-19 14:13:39 -0500

    Show all memberships on leader board

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -5,7 +5,7 @@ class LeaderboardController < ApplicationController
     authorize! :read, current_league
     @leaders = current_league.memberships.select("memberships.*, row_number() over (order by (memberships.balance + memberships.outstanding_bets_value) DESC) as leaderboard_rank")
                                          .includes(:user)
-                                         .order("(memberships.balance + memberships.outstanding_bets_value) DESC").limit(10)
+                                         .order("(memberships.balance + memberships.outstanding_bets_value) DESC")
   end
 
 end
